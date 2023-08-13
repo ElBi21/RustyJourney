18d044f3c494d6c4cea6e6b928ada0d5be97ca67
ElBi21 <64312343+ElBi21@users.noreply.github.com>
2023-08-13 12:19:38 +0200

    Continued 04 - Structs

--- 04 - Structs/structs/src/main.rs
+++ 04 - Structs/structs/src/main.rs
@@ -5,6 +5,6 @@ use structs_general::structs_general;
 use structs_ownership::structs_ownership_and_examples;
 
 fn main() {
-    // structs_general();
+    structs_general();
     structs_ownership_and_examples()
 }
