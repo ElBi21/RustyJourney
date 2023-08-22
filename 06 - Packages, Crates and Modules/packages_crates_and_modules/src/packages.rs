1a55d4b5af8429201f4e8d2c8c7d66cda403b0d9
ElBi21 <64312343+ElBi21@users.noreply.github.com>
2023-08-22 19:23:32 +0200

    Added 06 - Packages, Crates and Modules

---
+++ 06 - Packages, Crates and Modules/packages_crates_and_modules/src/packages.rs
@@ -0,0 +1,14 @@
+/// **Packages and Crates**
+///
+/// In Rust, a **crate** is the smallest amount of code that is considered at a time, during compile
+/// time, by the Rust compiler. It comes in two types:
+///  - **Binary crate**: it's a crate that compiles to a runnable executable, and it could be a
+///                      command line program or, for instance, a server. Each binary crate has its
+///                      own `main()` function;
+///  - **Library crate**: this kind of crate doesn't have any `main()` function, but it's instead
+///                       meant to hold extensible functions (an example could be the `Matplotlib`
+///                       package for `Python`).
+/// The word **crate** is mostly used to refer to **library crates**
+fn packages() {
+
+}
