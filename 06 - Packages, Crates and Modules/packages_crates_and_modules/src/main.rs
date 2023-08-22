37b4910794e07f3b1d74889ebc1bd069737e0137
ElBi21 <64312343+ElBi21@users.noreply.github.com>
2023-08-22 19:23:37 +0200

    Added 06 - Packages, Crates and Modules

---
+++ 06 - Packages, Crates and Modules/packages_crates_and_modules/src/main.rs
@@ -0,0 +1,17 @@
+/// As one proceeds in writing a project, it may be normal to want to split the code into multiple
+/// files, may it be for clearance, for keeping order, or for the need of modularity. Rust allows
+/// for that.
+///
+/// A lot of it works with the concept of "scope": in a scope, two elements can't have the same
+/// name, but we can allow or deny the use or access of some items inside scopes.
+///
+/// There are 4 important features that Rust adopts for splitting the code into multiple parts:
+///  - **Packages**: this feature allows for the creation, sharing and use of crates, as well as
+///                  building and testing;
+///  - **Crates**: a tree of modules, which ultimately creates a library;
+///  - **Modules and use**: Allows to control the organization, scope and privacy of the various
+///                         paths in the code;
+///  - **Paths**: The way of naming an item (be it a function, a variable, ...).
+fn main() {
+    println!("Hello, world!");
+}
